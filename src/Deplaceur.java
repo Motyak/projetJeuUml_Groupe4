1df92b0ed3c5162ba76a496ef29f32d62f05535e
Tommy Anfosso <anfossotommy@hotmail.fr>
2019-12-29 18:22:22 +0100

    deplaceur termine

--- src/Deplaceur.java
+++ src/Deplaceur.java
@@ -1,6 +1,8 @@
+import java.nio.channels.FileLockInterruptionException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Deplaceur {
@@ -12,11 +14,14 @@ public class Deplaceur {
 		this.plateau=p;
 	}
 	
-	public void deplacer(Personnage p,Pair<Integer,Integer> direction)
+	public boolean deplacer(Personnage p,Pair<Integer,Integer> direction)
 	{
 		int dimension = this.plateau.getDimension();
 		int indexDepart = Plateau.coordsToIndex(p.getCoords(), dimension);
 		Pair<Integer,Integer> coordsArrivee = new Pair<Integer,Integer>(p.getCoords().key+direction.key,p.getCoords().value+direction.value);
+//		si coords en dehors du plateau => return false
+		if(coordsArrivee.key<0 || coordsArrivee.key>dimension-1 || coordsArrivee.value<0 || coordsArrivee.value>dimension-1)
+			return false;
 		int indexArrivee = Plateau.coordsToIndex(coordsArrivee, dimension);
 		
 //		retirer le perso de la case ou il se trouve
@@ -28,17 +33,32 @@ public class Deplaceur {
 		
 //		mettre a jour ses coordonnées
 		p.setCoords(coordsArrivee);
+		
+		return true;
 	}
 	
-	public void deplacerAleat(Personnage p, ArrayList<Pair<Integer,Integer>> dirPossibles, int nbMaxDeplacements)
+	public void deplacerAleat(Personnage p, List<Pair<Integer,Integer>> dirPossibles, int nbMaxDeplacements)
 	{
-//		ca va utiliser deplacer (deplacement manuel)
+		Random r = new Random();
 		
+//		generer nombre de deplacements
+		int nbDeplacements = r.nextInt(nbMaxDeplacements)+1;	//genere un nb entre 1 (compris) et nbMaxDeplacements (compris)
+		System.out.println(nbDeplacements);
 		
+		for(int i = 1;i<=nbDeplacements;++i)
+		{
+			boolean impossible=true;
+			while(impossible)
+			{
+				Pair<Integer,Integer> direction = dirPossibles.get(r.nextInt(dirPossibles.size()));
+				System.out.println(direction.key+";"+direction.value);
+				impossible = !deplacer(p, direction);
+			}
+		}
 	}
 	
 	public static void main(String args[])
-	{
+	{	
 		Plateau plateau;
 		List<Corsaire> corsaires = Arrays.asList(new Corsaire("_J1_"),new Corsaire("_J2_"));
 		List<Pirate> pirates = Arrays.asList(new Boucanier(),new Flibustier());
@@ -54,10 +74,11 @@ public class Deplaceur {
 			sc.close();
 			Personnage persoADeplacer = plateau.getCases().get(a).getPersos().get(0);
 			
-			d.deplacer(persoADeplacer, Direction.HAUT);
-			d.deplacer(persoADeplacer, Direction.GAUCHE);
-			d.deplacer(persoADeplacer, Direction.DROITE);
-			d.deplacer(persoADeplacer, Direction.BAS);
+//			d.deplacer(persoADeplacer, Direction.HAUT);
+//			d.deplacer(persoADeplacer, Direction.GAUCHE);
+//			d.deplacer(persoADeplacer, Direction.BAS_DROITE);
+			
+			d.deplacerAleat(persoADeplacer, Direction.LIGNES_DROITES_ET_DIAG, 3);
 			
 			plateau.afficher();		
 
--- src/Direction.java
+++ src/Direction.java
@@ -13,4 +13,5 @@ public class Direction {
 	
 	public static final List<Pair<Integer, Integer>> LIGNES_DROITES = Arrays.asList(HAUT,BAS,GAUCHE,DROITE);
 	public static final List<Pair<Integer, Integer>> DIAGONALES = Arrays.asList(HAUT_GAUCHE,HAUT_DROITE,BAS_GAUCHE,BAS_DROITE);
+	public static final List<Pair<Integer, Integer>> LIGNES_DROITES_ET_DIAG = Arrays.asList(HAUT,BAS,GAUCHE,DROITE,HAUT_GAUCHE,HAUT_DROITE,BAS_GAUCHE,BAS_DROITE);
 }
